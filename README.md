bd3477b81c7bd988cc73cb9d9efd7b9f518bba5f
Teodora Ilic <teodora.damnjanovic@monolite.tech>
2024-06-10 20:52:13 +0200

    Add data retrieval functionality for product page

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # smile-bi-interview
+
+This project is a web scraper that fetches product data from a website using Puppeteer.
+
+## Setup
+
+1. Clone the repository:
+
+git clone https://github.com/TeodoraDamnjanovic/smile-bi-interview.git
+
+2. Navigate to the project directory:
+
+cd smile-bi-interview
+
+3. Install the dependencies:
+
+npm install
+
+4. Run Kafka:
+
+docker-compose up -d
+
+5. To run the web scraper, use the following command:
+
+node index.js
+
+6. Run to get kafka-container-id:
+
+docker ps
+
+7. Consume Kafka messages:
+
+docker exec -it <kafka-container-id> /opt/kafka/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic bayer-data --from-beginning
+
+8. Stop Kafka:
+
+docker-compose down
+
+## Output data
+
+This will fetch the product data and write it to a file named output.json, as well as send data to Kafka.
+The output data is a JSON array where each item is an object with the following structure:
+{
+    "price": "string",
+    "shop_name": "string",
+    "position": "number"
+}
+
+
+
+## Limitations
+
+1. Unable to Click Button to Retrieve More Data:
+
+On the page displaying all products, there is a button intended to load more data. However, the application was unable to click this button successfully. This limitation may be due to changes in the page's DOM structure, timing issues, or restrictions imposed by the webpage.
+As a workaround, the application processes the data available on the initial load of the page.
+
+2. Dockerization Issues:
+
+The intention was to dockerize the Node.js application and include it in a Docker Compose configuration to allow for the entire setup to be run with a single command. However, when the browser is launched in headless mode (headless: true), the program does not function as expected.
+This issue may be related to the headless browser environment not handling certain operations the same way a non-headless browser would. Debugging and fixing this would require more time and possibly alternative approaches or configurations.
+As a result, the application is not currently running in a Docker container, and must be run manually in a development environment.
---
+++ index.js
@@ -0,0 +1,133 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const kafka = require('kafka-node');
+
+function validateData(data) {
+    
+    //I've tested the regex in inspect and it works !/^\d+,\d{2} €$/.test(data.price)
+    //Don't know why it is not working here
+    //I'll provide a solution without regex
+    if (typeof data.shop_name !== 'string') {
+        throw new Error('Invalid price');
+    }
+
+    if (typeof data.shop_name !== 'string' || data.shop_name.trim() === '') {
+        throw new Error('Invalid shop name');
+    }
+
+    if (typeof data.position !== 'number' || data.position <= 0) {
+        throw new Error('Invalid position');
+    }
+}
+
+function validateItems(items) {
+    items.forEach((item, index) => {
+        try {
+            validateData(item);
+        } catch (error) {
+            console.error(`Item ${index + 1} is invalid: ${error.message}`);
+            throw new Error('Invalid items');
+        }
+    });
+}
+
+function writeToFile(filename, data) {
+    fs.writeFile(filename, data, (err) => {
+        if (err) {
+            throw new Error('Error writing to file:', err);
+        } else {
+            console.log('Successfully wrote to file');
+        }
+    });
+}
+
+function sendToKafka(topic, data) {
+    const client = new kafka.KafkaClient({kafkaHost: 'localhost:9092'});
+    const producer = new kafka.Producer(client);
+    
+    producer.on('ready', () => {
+        const payloads = [
+            { topic: topic, messages: JSON.stringify(data) },
+        ];
+        producer.send(payloads, (err, data) => {
+            if (err) {
+                console.error('Error sending data to Kafka:', err);
+            } else {
+                console.log('Successfully sent data to Kafka:', data);
+            }
+        });
+
+        producer.close(() => {
+            console.log('Producer closed');
+        });
+    });
+
+    producer.on('error', (err) => {
+        console.error('Error with Kafka producer:', err);
+    });
+}
+
+async function fetchItems(page) {
+    const items = await page.$$eval('li.productOffers-listItem', (nodes) => nodes.map((node, index) => {
+        const priceWithCurrency = node.querySelector('.productOffers-listItemOfferPrice').innerText;
+        const price = priceWithCurrency.split('\n')[0].trim().split(' ')[0];
+        const shop_name = node.querySelector('a.productOffers-listItemOfferShopV2LogoLink').getAttribute('data-shop-name');
+        return { price, shop_name, position: index + 1 };
+    }));
+
+    return items;
+}
+
+async function fetchBayerProducts() {
+    let browser;
+    try {
+        browser = await puppeteer.launch({
+            headless: false,
+            defaultViewport: null,
+            args: [
+                '--no-sandbox',
+            ],
+        });
+
+        const page = await browser.newPage();
+        page.setDefaultNavigationTimeout(0);
+        
+        page.on('error', error => {
+            console.error('An error occurred on the page:', error);
+        });
+        
+        page.on('pageerror', error => {
+            console.error('An uncaught exception happened within the page:', error);
+        });
+
+        const response =  await page.goto('https://www.idealo.de/preisvergleich/OffersOfProduct/201846460_-aspirin-plus-c-forte-800-mg-480-mg-brausetabletten-bayer.html');
+        
+        if (response.status() === 403) {
+            console.error('Access Denied');
+            throw new Error('Access Denied');
+        }
+
+        await page.setViewport({
+            width: 1200,
+            height: 800
+        });
+
+        const items = await fetchItems(page);
+        console.log(items);
+
+        validateItems(items);
+
+        writeToFile('output.json', JSON.stringify(items));
+
+        sendToKafka('bayer-data', items);
+    } catch (error) {
+        console.error(error);
+    } finally {
+        if(browser) {
+            console.log("Browser closed");
+            browser.close();
+        }
+    }
+}
+
+fetchBayerProducts();
